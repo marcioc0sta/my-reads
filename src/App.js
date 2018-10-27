d52b5c7ae35c6c55b4465d537d00a20e4620820e
Márcio Costa <marciocst2@gmail.com>
2018-10-27 19:41:51 -0300

    feat(Initial structure) Add the initial structure and load all books

--- src/App.js
+++ src/App.js
@@ -3,7 +3,40 @@ import { Route } from 'react-router-dom';
 
 import MainView from './views/MainView/MainView';
 import SearchView from './views/SearchView/SearchView';
+
+import * as BooksAPI from './api/BooksAPI';
 class App extends Component {
+  state = {
+    booksShelf: {
+      currentlyReading: [],
+      wantToRead: [],
+      read: [],
+      isLoading: true,
+    },
+  }
+
+  componentDidMount() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    BooksAPI.getAll().then(booksList => {
+      booksList.forEach(book => {
+        this.organizeShelfs(book);
+      });
+    });
+  }
+
+  organizeShelfs(book) {
+    this.setState(prevState => ({
+      booksShelf: {
+        ...prevState.booksShelf,
+        [book.shelf]: prevState.booksShelf[book.shelf].concat(book),
+        isLoading: false,
+      }
+    }));
+  }
+
   render() {
     return (
       <div className="App">
